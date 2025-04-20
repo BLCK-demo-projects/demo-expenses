68e980da3e045801b9da04baf3b9e9072e70ebfe
BLCK <123077751+BLCK-B@users.noreply.github.com>
2025-04-20 08:37:58 +0200

    init

---
+++ src/test/java/com/blck/demo_expenses/DemoExpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blck.demo_expenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoExpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
